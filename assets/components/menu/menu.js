3920a9efc1de21f2e74ba6b2039673b76584e304
root <grzegorzchrymecki@php-developer.ie>
2021-03-31 20:26:39 +0100

    last ok

--- assets/components/menu/menu.js
+++ assets/components/menu/menu.js
@@ -7,5 +7,24 @@ $('#nav-button').on('click', (e)=>{
 $('#nav-close').on('click', ()=>{
     $('#nav-mobile').slideToggle();
     $('#nav-button').show();
+});
+/*
+Function that read menu Links from backend
+ */
+const menuDesktop = document.querySelectorAll('.nav__desktop__list > li > a');
+const menuMobile = document.querySelectorAll('.nav__mobile__list >li > a');
+const linksText = ['home5', 'link12','link2', 'link3'];
 
+/*
+Function Responsible for loading Menu link from variable
+ */
+function generateLinkText(element, linkText){
+    element.innerHTML = linkText;
+}
+
+menuDesktop.forEach((menuItem, key)=>{
+    generateLinkText(menuItem, linksText[key]);
+});
+menuMobile.forEach((menuItem, key)=>{
+    generateLinkText(menuItem, linksText[key]);
 });
