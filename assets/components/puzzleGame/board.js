e439e5f74f7bba9cf9b64b7c66d7800dcd7cad05
root <grzegorzchrymecki@php-developer.ie>
2021-04-02 18:33:32 +0100

    ok

--- assets/components/puzzleGame/board.js
+++ assets/components/puzzleGame/board.js
@@ -1,12 +1,21 @@
 const pieces = document.querySelectorAll('.puzzle__piece');
 let index = 0;
+// Init board
+let board = [];
+for(a=0; a<3; a++){
+    board[a] = [];
+    for(b=0;b<3;b++){
+        board[a][b] = 0;
+    }
+}
 for(row=0; row<3;row++){
     for(col=0; col <3; col++){
         if(index <= 7){
             pieces[index].style.left = `${col * 85}px`;
             pieces[index].style.top = `${row * 85}px`;
+            board[row][col] = index;
             index++;
         }
     }
 }
-
+console.log(board);
