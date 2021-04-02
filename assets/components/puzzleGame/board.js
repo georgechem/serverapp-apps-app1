0f3cc924a70dc4f8f65ee42bf9b38325deb50e15
root <grzegorzchrymecki@php-developer.ie>
2021-04-02 21:36:45 +0100

    ok

--- assets/components/puzzleGame/board.js
+++ assets/components/puzzleGame/board.js
@@ -9,7 +9,7 @@ for(row=0; row<3;row++){
         if(index <= 7){
             pieces[index].style.left = `${col * 85}px`;
             pieces[index].style.top = `${row * 85}px`;
-            board[row][col] = index;
+            board[row][col] = index + 1;
             index++;
         }else{
             board[row][col] = -1;
@@ -17,3 +17,53 @@ for(row=0; row<3;row++){
     }
 }
 console.log(board);
+pieces.forEach((piece)=>{
+    piece.addEventListener('click', (e)=>{
+        const elementNo = parseInt(e.target.innerHTML,10);
+        let isIn = [];
+        let number = 0;
+        board.forEach((item, key)=>{
+            isIn.push(item.findIndex((element)=>{
+                return element === elementNo;
+            }));
+            number = isIn.pop();
+            if(number >= 0){
+                isIn.push([key, number]);
+            }
+        })
+        /**
+         * isIn[row, col] - format = value at certain position
+         */
+        /**
+         * 1.check is move is possible
+         * 2. do move on virtual board
+         * 3. do move on screen as animation
+         */
+        const clickedCoordX = isIn[0][0];
+        const clickedCoordY = isIn[0][1];
+        console.log(board[clickedCoordX][clickedCoordY]);
+        // create matrix of possibilities
+        const matrix = [];
+        if((clickedCoordX - 1) >= 0){
+            matrix.push([clickedCoordX - 1, clickedCoordY]);
+        }
+        if((clickedCoordX + 1) <= 2){
+            matrix.push([clickedCoordX + 1, clickedCoordY]);
+        }
+        if((clickedCoordY - 1) >= 0){
+            matrix.push([clickedCoordX, clickedCoordY - 1]);
+        }
+        if((clickedCoordY + 1) <= 2){
+            matrix.push([clickedCoordX, clickedCoordY + 1]);
+        }
+        // check if certain move selected - check
+        // availability of -1 value in gives matrix
+        // check value -1
+        matrix.forEach((coordinates) => {
+            console.log(board[coordinates[0]][coordinates[1]]);
+            //console.log(coordinates);
+        });
+
+
+    });
+});
