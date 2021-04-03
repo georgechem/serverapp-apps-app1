f642b77c57602f32b77d7f6b872a879bf3395970
root <grzegorzchrymecki@php-developer.ie>
2021-04-03 21:35:59 +0100

    fixed

--- assets/components/puzzleGameOne/puzzleGameOneControll.js
+++ assets/components/puzzleGameOne/puzzleGameOneControll.js
@@ -5,15 +5,14 @@
  */
 function createPiece(id, amount = 1){
     const pieces = [];
-    let piece = null;
-    for(s=0; s < amount; amount++){
-        piece = document.createElement('div');
+    for(s=0; s < amount; s++){
+        let piece = document.createElement('div');
         piece.id = `id${id}${s}`;
         piece.style.position = 'absolute';
         piece.style.width = '40px';
         piece.style.height = '40px';
         piece.style.margin = '5px';
-        piece.style.backgroundColor = '#999';
+        piece.style.backgroundColor = '#999999';
         piece.style.borderRadius= '5px';
         piece.style.border = '1px solid #ccc';
         pieces.push(piece);
@@ -32,8 +31,8 @@ function mainThread(){
     const board = document.getElementById('board');
     board.style.height = '300px';
 
-    const piece = createPiece('piece01');
-    appendPiece(piece, board);
+    const pieces = createPiece('piece01');
+    appendPiece(pieces, board);
 
     console.log(board);
 
