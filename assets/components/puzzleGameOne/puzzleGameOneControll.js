f745b527a2d71f4b65f0c2de7a12b9eafedbef63
root <grzegorzchrymecki@php-developer.ie>
2021-04-04 10:16:05 +0100

    ok

--- assets/components/puzzleGameOne/puzzleGameOneControll.js
+++ assets/components/puzzleGameOne/puzzleGameOneControll.js
@@ -3,19 +3,33 @@
  * @param id
  * @returns {HTMLDivElement}
  */
-function createPiece(id, amount = 1){
+function createPiece(id, amount = 1, currentBoardWidth = 200){
     const pieces = [];
-    const colors = ['red', 'green', 'blue', 'black', 'white'];
+    const colors = ['#f00', '#0f0', '#00f', '#000'];
+    const pieceSide = 40;
+    const pieceMargin = 5;
+    const rowLength = currentBoardWidth/(pieceSide+2*pieceMargin);
+    const pieceStep = currentBoardWidth/rowLength;
+    const rowPosition = [];
+    for(start = 0;start < currentBoardWidth ;start += pieceStep){
+        rowPosition.push(start);
+    }
     for(s=0; s < amount; s++){
         let piece = document.createElement('div');
         piece.id = `id${id}${s}`;
         piece.style.position = 'absolute';
-        piece.style.width = '40px';
-        piece.style.height = '40px';
-        piece.style.margin = '5px';
-        piece.style.backgroundColor = `${colors[Math.floor(Math.random()*5)]}`;
+        piece.style.width = `${pieceSide}px`;
+        piece.style.height = `${pieceSide}px`;
+        piece.style.margin = `${pieceMargin}px`;
+        piece.style.border = `1px solid #222`;
+        const currentColor = colors[Math.floor(Math.random()*4)];
+        if(currentColor === '#000'){piece.style.border = `1px solid #aaa`;}
+        piece.style.backgroundImage = `radial-gradient( #777 ,${currentColor} )`;
+
+        //piece.style.backgroundColor = `${colors[Math.floor(Math.random()*5)]}`;
         piece.style.borderRadius= '5px';
-        //piece.style.border = '1px solid #ccc';
+        piece.style.top = '0px';
+        piece.style.left = `${rowPosition[Math.floor(Math.random()*rowLength)]}px`
         pieces.push(piece);
     }
 
@@ -29,13 +43,15 @@ function appendPiece(pieces, location){
 }
 
 function mainThread(){
+    const boardWidth = 300;
+    const gameBox = document.getElementById('gameBox');
+    gameBox.style.width = `${boardWidth+20}px`;
     const board = document.getElementById('board');
     board.style.height = '400px';
+    board.style.width = `${boardWidth}px`;
 
-    const pieces = createPiece('piece01');
+    const pieces = createPiece('piece01',1, boardWidth);
     appendPiece(pieces, board);
-
-    console.log(board);
 
 
 }
